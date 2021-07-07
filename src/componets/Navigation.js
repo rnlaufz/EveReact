1745bc06ecf9f5994cee92eb0703247f942dc2a1
Regina Zaripova <rnloftz@gmail.com>
2021-07-07 21:17:18 +1000

    created main pages and util components | linked router

---
+++ src/componets/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+ const Navigation = () => {
+    return (
+        <div>
+            <h3>Navigation</h3>
+        </div>
+    )
+}
+
+
+export default Navigation
---
+++ src/pages/Book.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Book = () => {
+    return (
+        <div>
+            <h4>Book page creation process</h4>
+        </div>
+    )
+}
+
+
+export default Book;

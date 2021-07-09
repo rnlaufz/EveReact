0a3e700056e587936fc8872222053b84dcc1d3e6
Regina Zaripova <rnloftz@gmail.com>
2021-07-09 23:02:59 +1000

    deleted extra files | added book page markup with styling

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -1,9 +1,63 @@
 import React from 'react';
+import Footer from '../componets/Footer';
+import Navigation from '../componets/Navigation';
 
 const Book = () => {
     return (
-        <div>
-            <h4>Book page creation process</h4>
+        <div className="book-container">
+            <Navigation/>
+            <div className="card">
+
+<div className="form-container">
+    <form id="form">
+        
+        <div id="dates" className="dates-form-group">
+            <h3>Select your dates:</h3>
+            <div className="inputs-flex">
+            <input type="date" id="arrival"  name="arrival" />
+            <input type="date" id="departure"  name="departure" />
+        </div>
+          </div>
+          <div className="selector" id="selector">
+              <h3>Select your house:</h3>
+              <select name="options" id="options">
+                  <option value="Lake Cabin" id="cabin-one" className="default">Lake Cabin</option>
+                  <option value="Woods Cabin" id="cabin-two">Woods Cabin</option>
+                  <option value="Heels Cabin" id="cabin-three">Heels Cabin</option>
+                  
+                </select>
+                <div id="selected">
+                    <div className="description">
+                        <h4 id="selected-house-title">Lake Cabin</h4>
+                        <ul className="house-description" id="selected-house-description">
+                            <li className="house-description-item" id="selected-house-address">Neverland Lake, 02</li>
+                            <li className="house-description-item" id="selected-house-price">200$ per day</li>
+                            <li className="house-description-item" id="selected-house-facilities">Facilities: plumbing, electrisity, telephone, bath</li>
+                            <li className="house-description-item" id="selected-house-occupied">Occupied: no</li>
+                        </ul>
+                        <button id="selected-more" className="btn">View more</button>
+                    </div>
+                </div>
+          </div>
+          
+          <div className="summary-container">
+            <h4>Your summary</h4>
+            <div 
+            >
+            <ul className="form-list-group" id="summary-list">
+                <li className="form-list-group-item">Dates: from <span id="arrival-date" className="underlined"></span> to <span id="departure-date" className="underlined"></span> </li>
+                <li className="form-list-group-item">Amount of days: <span id="days-total" className="underlined">0</span></li>
+                <li className="form-list-group-item">Selected house: <span id="selected-house" className="underlined">Lake Cabin</span></li>
+                <li className="form-list-group-item">Address of house: <span id="address-of-selected-house" className="underlined">Neverland Lake, 02</span></li>
+                <li className="form-list-group-item">Price: <span id="total-price" className="underlined">0$</span></li>
+            </ul>
+        </div>
+        </div>
+        <button className="btn" type="submit">Leave a request</button>
+    </form>
+</div>
+</div>
+        <Footer/>
         </div>
     )
 }
